6ad8aaf99ab6c0c6e576a6e1876c37340e2a623e
AstraPlazma <2962558943@qq.com>
2026-04-08 11:36:36 +0000

    tr

--- examples/LIBERO/eval_files/eval_libero.sh
+++ examples/LIBERO/eval_files/eval_libero.sh
@@ -17,10 +17,10 @@ export MUJOCO_GL=osmesa
 export PYOPENGL_PLATFORM=osmesa
 
 host="127.0.0.1"
-base_port=5698
+base_port=5699
 unnorm_key="franka"
 # your_ckpt=./playground/Pretrained_models/Qwen3.5-0.8B/model.safetensors-00001-of-00001.safetensors
-your_ckpt=./results/Checkpoints/0402_libero4in1_qwen35gr00t/final_model/pytorch_model.pt
+your_ckpt=./results/Checkpoints/0402_libero4in1_qwen35gr00t/checkpoints/steps_20000_pytorch_model.pt
 
 # export DEBUG=true
 
@@ -32,8 +32,8 @@ LOG_DIR="logs/$(date +"%Y%m%d_%H%M%S")"
 mkdir -p ${LOG_DIR}
 
 
-task_suite_name=libero_10
-num_trials_per_task=20
+task_suite_name=libero_spatial
+num_trials_per_task=50
 video_out_path="results/${task_suite_name}/${folder_name}"
 
 
--- examples/LIBERO/eval_files/run_policy_server.sh
+++ examples/LIBERO/eval_files/run_policy_server.sh
@@ -2,10 +2,10 @@
 export PYTHONPATH=$(pwd):${PYTHONPATH} # let LIBERO find the websocket tools from main repo
 export star_vla_python=/home/user/miniconda3/envs/newvla/bin/python
 # your_ckpt=./playground/Pretrained_models/Qwen3.5-0.8B/model.safetensors-00001-of-00001.safetensors
-your_ckpt=./results/Checkpoints/0402_libero4in1_qwen35gr00t/final_model/pytorch_model.pt
+your_ckpt=./results/Checkpoints/0402_libero4in1_qwen35gr00t/checkpoints/steps_20000_pytorch_model.pt
 
-gpu_id=0,1,2,3,4,5,6
-port=5698
+gpu_id=0,1,2,3,4
+port=5699
 ################# star Policy Server ######################
 
 # export DEBUG=true
